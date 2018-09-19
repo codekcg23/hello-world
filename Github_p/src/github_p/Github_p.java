4a6a16e4a2c1edde4206ab1358adcbdea82f37cc
codekcg23 <43032723+codekcg23@users.noreply.github.com>
2018-09-19 15:35:32 +0530

    Update Github_p.java

--- Github_p/src/github_p/Github_p.java
+++ Github_p/src/github_p/Github_p.java
@@ -3,9 +3,6 @@ package github_p;
 
 public class Github_p {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         System.out.println("Hello world !!!");
