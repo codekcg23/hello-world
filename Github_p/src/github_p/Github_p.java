eab6ff4ffa4eac5715114594cee58dfe8b162ac4
codekcg23 <kavigamage23@gmail.com>
2018-09-19 15:25:15 +0530

    Hello_world_print

---
+++ Github_p/src/github_p/Github_p.java
@@ -0,0 +1,15 @@
+
+package github_p;
+
+public class Github_p {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world !!!");
+        System.out.println("GitHub Practice Test1");
+    }
+    
+}
